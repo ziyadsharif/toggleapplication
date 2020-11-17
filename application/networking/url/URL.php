06e077ccb04e8b5e9ab69c37ffb58033c2ce16f4
ziyadshariftoggle <65122707+ziyadshariftoggle@users.noreply.github.com>
2020-11-17 11:06:41 +0400

    test link

--- application/networking/url/URL.php
+++ application/networking/url/URL.php
@@ -8,14 +8,14 @@ class URL
     public $PATCH = 'PATCH';
     public $DELETE = 'DELETE';
 
-    public $toggleapi = 'https://toggleapi.herokuapp.com/api/';
+    public $toggleapi = 'http://togglemarketapis.uaenorth.cloudapp.azure.com:1337/api/';
     public $addprojectareaURL1 = "buyer-project/";
     public $addprojectareaURL2 = "projectArea/";
     public $getareatypesURL;
     public $getuserURL;
     public $getallprojectsURL;
     public $getboqbyprojectidURL;
-    public $updateProjectDetails = "https://toggleapi.herokuapp.com​/api​/buyer-project​/";
+    public $updateProjectDetails = "http://togglemarketapis.uaenorth.cloudapp.azure.com:1337/api​/buyer-project​/";
 
     public function __construct()
     {
@@ -35,25 +35,25 @@ class URL
         $this->getprojectsbyuseridURL = $this->toggleapi . $getprojectsbyuserid;
 
     }
-    public $loginURL =  "https://toggleapi.herokuapp.com/api/user/login";
-    public $forgotpasswordURL = "https://toggleapi.herokuapp.com/api/user/forgotPassword";
-    public $createuserURL =  "https://toggleapi.herokuapp.com/api/user/signup";
-    public $getallusersURL = "https://toggleapi.herokuapp.com/api/user/allUser";
+    public $loginURL =  "http://togglemarketapis.uaenorth.cloudapp.azure.com:1337/api/user/login";
+    public $forgotpasswordURL = "http://togglemarketapis.uaenorth.cloudapp.azure.com:1337/api/user/forgotPassword";
+    public $createuserURL =  "http://togglemarketapis.uaenorth.cloudapp.azure.com:1337/api/user/signup";
+    public $getallusersURL = "http://togglemarketapis.uaenorth.cloudapp.azure.com:1337/api/user/allUser";
 
-    public $deleteuserURL =  "https://toggleapi.herokuapp.com/api/user/";
-    public $updateuserinfoURL =  "https://toggleapi.herokuapp.com/api/user/";
-    public $updateuserURL =  "https://toggleapi.herokuapp.com/api/user/updateUser/";
-    public $changepasswordURL =  "https://toggleapi.herokuapp.com/api/user/changePassword";
-    public $sendemailURL =  "https://toggleapi.herokuapp.com/api/email/send/";
-    public $getcountriesURL =  "http://toggleapi.herokuapp.com/api/country";
-    public $gettypeURL =  "http://toggleapi.herokuapp.com/api/type";
-    public $getproducts =  "https://toggleapi.herokuapp.com/api/product/search";
-    public $suggestiveSearch =  "https://toggleapi.herokuapp.com/api/product/";
+    public $deleteuserURL =  "http://togglemarketapis.uaenorth.cloudapp.azure.com:1337/api/user/";
+    public $updateuserinfoURL =  "http://togglemarketapis.uaenorth.cloudapp.azure.com:1337/api/user/";
+    public $updateuserURL =  "http://togglemarketapis.uaenorth.cloudapp.azure.com:1337/api/user/updateUser/";
+    public $changepasswordURL =  "http://togglemarketapis.uaenorth.cloudapp.azure.com:1337/api/user/changePassword";
+    public $sendemailURL =  "http://togglemarketapis.uaenorth.cloudapp.azure.com:1337/api/email/send/";
+    public $getcountriesURL =  "http://togglemarketapis.uaenorth.cloudapp.azure.com:1337api/country";
+    public $gettypeURL =  "http://togglemarketapis.uaenorth.cloudapp.azure.com:1337api/type";
+    public $getproducts =  "http://togglemarketapis.uaenorth.cloudapp.azure.com:1337/api/product/search";
+    public $suggestiveSearch =  "http://togglemarketapis.uaenorth.cloudapp.azure.com:1337/api/product/";
 
 
-    public $updatepersonalinfoURL =  "http://toggleapi.herokuapp.com/api/user/personalInfo";
-    public $updatecontactinfoURL =  "http://toggleapi.herokuapp.com/api/user/contactInfo";
-    public $updatecompanydetailsURL = "http://toggleapi.herokuapp.com/api/user/companyDetails/";
+    public $updatepersonalinfoURL =  "http://togglemarketapis.uaenorth.cloudapp.azure.com:1337api/user/personalInfo";
+    public $updatecontactinfoURL =  "http://togglemarketapis.uaenorth.cloudapp.azure.com:1337api/user/contactInfo";
+    public $updatecompanydetailsURL = "http://togglemarketapis.uaenorth.cloudapp.azure.com:1337api/user/companyDetails/";
 
     public $getallproducts =  "http://localhost:3000/api/product/getallproducts";
     public $getproductsbypage =  "http://localhost:3000/api/product/getproductsbypage/";
@@ -61,14 +61,14 @@ class URL
     public $getproductbyproductid =  "http://localhost:3000/api/product/getproductbyproductid/";
     public $getproductsbycategoryid =  "http://localhost:3000/api/product/getproductsbycategoryid/";
 
-    public $getAreaTypes = "https://toggleapi.herokuapp.com/api/areatype";
-    public $addProject = "https://toggleapi.herokuapp.com/api/buyer-project";
+    public $getAreaTypes = "http://togglemarketapis.uaenorth.cloudapp.azure.com:1337/api/areatype";
+    public $addProject = "http://togglemarketapis.uaenorth.cloudapp.azure.com:1337/api/buyer-project";
 
-    public $createEvent = "https://toggleapi.herokuapp.com/api/event";
-    public $getProjectInterests = "https://toggleapi.herokuapp.com/api/projectinterest";
-    public $getEvents = "https://toggleapi.herokuapp.com/api/event";
+    public $createEvent = "http://togglemarketapis.uaenorth.cloudapp.azure.com:1337/api/event";
+    public $getProjectInterests = "http://togglemarketapis.uaenorth.cloudapp.azure.com:1337/api/projectinterest";
+    public $getEvents = "http://togglemarketapis.uaenorth.cloudapp.azure.com:1337/api/event";
 
-    public $getProjectDetails = "https://toggleapi.herokuapp.com/api/buyer-project/";
+    public $getProjectDetails = "http://togglemarketapis.uaenorth.cloudapp.azure.com:1337/api/buyer-project/";
 
     public function addprojectURL($projectId)
     {
@@ -84,8 +84,8 @@ class URL
 
         return $this->getareatypesbyprojectidURL;
     }
-    public $editProject="https://toggleapi.herokuapp.com/api/buyer-project";
-    public $getBuyerProjectsByUserId = "https://toggleapi.herokuapp.com/api/buyer-project/userId";
-    public $getUserPersonalIno = "https://toggleapi.herokuapp.com/api/user/personalInfo";
-    public $getUserContactInfo = "https://toggleapi.herokuapp.com/api/user/contactInfo";
+    public $editProject="http://togglemarketapis.uaenorth.cloudapp.azure.com:1337/api/buyer-project";
+    public $getBuyerProjectsByUserId = "http://togglemarketapis.uaenorth.cloudapp.azure.com:1337/api/buyer-project/userId";
+    public $getUserPersonalIno = "http://togglemarketapis.uaenorth.cloudapp.azure.com:1337/api/user/personalInfo";
+    public $getUserContactInfo = "http://togglemarketapis.uaenorth.cloudapp.azure.com:1337/api/user/contactInfo";
 }
